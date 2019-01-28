90ca4fa6988ba2dbf479a36a28d5000104a74326
Eduardo Namba <eduardo.namba@br.clara.net>
2019-01-28 20:00:28 -0200

    exemplo de uso do blueprint de vpc

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">=0.11"
+
+  # backend "s3" {
+  #   bucket = "654040296502-tfstate"
+  #   key    = "terraform/k8s-sao.tfstate"
+  #   region = "us-east-1"
+  # }
+}
---
+++ data.tf
@@ -0,0 +1,5 @@
+variable "account" {
+  type        = "string"
+  description = "Conta da credibilit-tst"
+  default     = "495770326048"
+}
